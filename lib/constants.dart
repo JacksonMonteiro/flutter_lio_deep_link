7122f8bf6fe0314b5d92e0737270edff2ca646f9
jacksonmonteiro <infor.jackson324@gmail.com>
2023-01-25 11:08:28 -0300

    remoção da chave de dev

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const ACCESS_TOKEN = "ZjpVpQiUNcO4BLUsnSmwqFCL8AExTqTEZ9kl9vm9lDAdnd3MOy";
-  static const CLIENT_ID = "loIDeZOSihHEaDmutRHUPl3dxJzKIQTgHJ5yncMHqLUNf241vz";
+  static const ACCESS_TOKEN = "";
+  static const CLIENT_ID = "";
 }
